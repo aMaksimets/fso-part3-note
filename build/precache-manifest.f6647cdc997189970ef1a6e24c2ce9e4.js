5599240d55d39bf53179291b107cac4ff72d1feb
Alexey Maksimets <54171298+aMaksimets@users.noreply.github.com>
2022-02-22 17:33:36 +1100

    init

---
+++ build/precache-manifest.f6647cdc997189970ef1a6e24c2ce9e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41dc74b3ff6459c1e29623d656af582",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b67faad8d1c3226c6af",
+    "url": "/static/css/main.2ab5a27c.chunk.css"
+  },
+  {
+    "revision": "85bf97f22fd4961a511d",
+    "url": "/static/js/2.47fcf83e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.47fcf83e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b67faad8d1c3226c6af",
+    "url": "/static/js/main.380914cd.chunk.js"
+  },
+  {
+    "revision": "bf2047133e1934337920",
+    "url": "/static/js/runtime-main.92c9d209.js"
+  }
+]);
